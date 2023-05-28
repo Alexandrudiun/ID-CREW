870a515068b5288e1e42b9bd4b6d56fc1dd0e9dc
Alexandru Dinu <76495571+Alexandrudiun@users.noreply.github.com>
2023-05-28 01:41:06 -0700

    Delete livrabile/source cod directory

--- livrabile/source cod/backend/conn.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$dbServername="localhost";
-$dbUsername="u312656249_idcrew";
-$dbPassoword="IonescuDinuCrew2023!";
-// $dbUsername="root";
-// $dbPassoword="";
-$dbName="u312656249_idcrew";
-
-$conn= mysqli_connect($dbServername, $dbUsername, $dbPassoword, $dbName);
--- livrabile/source cod/js/delete.js
+++
@@ -1,6 +0,0 @@
-  function deleteSlide(slideId) {
-    const slide = document.getElementById(slideId);
-    const slideItem = slide.parentNode;
-    slideItem.parentNode.removeChild(slideItem);
-  }
-
